6bf14f79ab37f43d24f77d8f97ec25b4c079b593
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-10-01 17:48:11 +0900

    'fix:etag.intercepotr'

--- src/common/interceptors/no-store.interceptor.ts
+++ src/common/interceptors/no-store.interceptor.ts
@@ -4,10 +4,16 @@ import {
   Injectable,
   NestInterceptor,
 } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class NoStoreIfAuthedInterceptor implements NestInterceptor {
-  intercept(context: ExecutionContext, next: CallHandler) {
+export class NoStoreIfAuthedInterceptor
+  implements NestInterceptor<unknown, unknown>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<unknown>,
+  ): Observable<unknown> {
     const res = context.switchToHttp().getResponse();
     res.setHeader('Cache-Control', 'no-store, private');
     res.setHeader('Vary', 'Authorization');
