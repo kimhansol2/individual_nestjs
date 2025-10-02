f35af4c7ece68d4ce00811b7bd034fcacf858200
kimhansol2 <119918073+kimhansol2@users.noreply.github.com>
2025-10-02 21:54:26 +0900

    'fix:steam-openid.service'

--- src/auth/steam-openid.service.ts
+++ src/auth/steam-openid.service.ts
@@ -10,7 +10,7 @@ import { UsersRepository } from 'src/domain/users/users.repository';
 import { User } from 'src/domain/users/user.entity';
 import { UnauthorizedException } from '@nestjs/common';
 import { CacheAsideService } from 'src/common/cache/cache-aside.service';
-import { myGamesCountIdx, myGamesIdx, profileIdx } from 'src/common/cache/keys';
+import { myGamesIdx, profileIdx } from 'src/common/cache/keys';
 import { OwnedGameRepository } from 'src/domain/games/owned-game.repository';
 
 const OP = 'https://steamcommunity.com/openid/login';
@@ -153,14 +153,14 @@ export class SteamOpenIdService {
 
     if (steamKey) {
       try {
-        const rows = await this.ownedRepo.fetchOwnedGamesAsRows(steamKey, user);
-        if (rows.length) {
-          await this.ownedRepo.upsertMany(rows);
-          await Promise.allSettled([
-            this.cache.invalidateByIndex(myGamesIdx(user.id)),
-            this.cache.invalidateByIndex(myGamesCountIdx(user.id)),
-          ]);
-        }
+        const { games, owned } = await this.ownedRepo.fetchOwnedGamesAsRows(
+          steamKey,
+          user,
+        );
+
+        await this.ownedRepo.upsertGames(games);
+        await this.ownedRepo.upsertOwnedMany(owned);
+        await this.cache.invalidateByIndex(myGamesIdx(user.id));
       } catch {
         /*..*/
       }
